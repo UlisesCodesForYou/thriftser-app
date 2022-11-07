34c0f76102ef8b41cee5593a539764f35ea02031
Ulises Orozco <ulisesorozco6@gmail.com>
2022-11-06 20:57:05 -0800

    Fixed spacing bug in the notifications folder.  Footer is correctly spaced at the botton and header is now fixed.

--- pages/notifications/notifications.tsx
+++ pages/notifications/notifications.tsx
@@ -4,59 +4,93 @@ import Link from "next/link";
 export const Notifications = () => {
     return (
         <>
-            {/*start: Social Network notification:light*/}
-
-            <div className="bg-blue-600 h-screen">
+            <div className="bg-blue-600 h-screen flex flex-col h-screen justify-between">
                 <div className="sticky w-full top-0 h-10">
-                    <header className='text-lg text-left font-bold text-white bg-blue-600 ml-4'>Notifications</header>
-                </div>
-
-                <div className="w-full h-auto relative bg-blue-600">
-                    <div className="bg-white  px-5 py-3.5 rounded-lg shadow hover:shadow-xl max-w-sm mx-auto transform hover:-translate-y-[0.125rem] transition duration-100 ease-linear mb-3">
-                        <div className="w-full flex items-center justify-between">
-                            <span className="font-medium text-sm text-slate-400">New Notification</span>
-                            <button className="-mr-1 bg-slate-100 hover:bg-slate-200 text-slate-400 hover:text-slate-600 h-5 w-5 rounded-full flex justify-center items-center">
-                                <svg className="h-2 w-2 fill-current items-center" viewBox="0 0 20 20"><path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/></svg>
-                            </button>
-                        </div>
-                        <div className="flex items-center mt-2 rounded-lg px-1 py-1 cursor-pointer">
-                            <div className="relative flex flex-shrink-0 items-end">
-                                <Image className="h-16 w-16 rounded-full" src="https://picsum.photos/id/1027/150/150" width={50} height={50}/>
-                                <span className="absolute h-4 w-4 bg-green-400 rounded-full bottom-0 right-0 border-2 border-white"></span>
+                    <header className='text-lg sticky text-left font-bold text-white bg-blue-900 text-center'>Notifications</header>
+                    <div className="w-full h-auto relative bg-blue-600">
+                        <div className="bg-white px-5 py-3.5 rounded-lg shadow hover:shadow-xl max-w-sm mx-auto mb-3 mt-3">
+                            <div className="w-full flex items-center justify-between">
+                                <span className="font-medium text-sm text-slate-400">New Notification</span>
+                                <button className="-mr-1 bg-slate-100 hover:bg-slate-200 text-slate-400 hover:text-slate-600 h-5 w-5 rounded-full flex justify-center items-center">
+                                    <svg className="h-2 w-2 fill-current items-center" viewBox="0 0 20 20"><path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/></svg>
+                                </button>
                             </div>
-                            <div className="ml-3">
-                                <span className="font-semibold tracking-tight text-xs">John Doe</span>
-                                <span className="text-xs leading-none opacity-50">reacted to your comment:</span>
-                                <p className="text-xs leading-4 pt-2 italic opacity-70">"This is the comment..."</p>
-                                <span className="text-[10px] text-blue-500 font-medium leading-4 opacity-75">a few seconds ago</span>
+                            <div className="flex items-center mt-2 rounded-lg px-1 py-1 cursor-pointer">
+                                <div className="relative flex flex-shrink-0 items-end">
+                                    <Image className="h-16 w-16 rounded-full" src="https://picsum.photos/id/1027/150/150" width={50} height={50}/>
+                                    <span className="absolute h-4 w-4 bg-green-400 rounded-full bottom-0 right-0 border-2 border-white"></span>
+                                </div>
+                                <div className="ml-3">
+                                    <span className="font-semibold tracking-tight text-xs">John Doe</span>
+                                    <span className="text-xs leading-none opacity-50">reacted to your comment:</span>
+                                    <p className="text-xs leading-4 pt-2 italic opacity-70">"This is the comment..."</p>
+                                    <span className="text-[10px] text-blue-500 font-medium leading-4 opacity-75">a few seconds ago</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="bg-white  px-5 py-3.5 rounded-lg shadow hover:shadow-xl max-w-sm mx-auto transform hover:-translate-y-[0.125rem] transition duration-100 ease-linear mb-3">
-                        <div className="w-full flex items-center justify-between">
-                            <span className="font-medium text-sm text-slate-400">New Notification</span>
-                            <button className="-mr-1 bg-slate-100 hover:bg-slate-200 text-slate-400 hover:text-slate-600 h-5 w-5 rounded-full flex justify-center items-center">
-                                <svg className="h-2 w-2 fill-current items-center" viewBox="0 0 20 20"><path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/></svg>
-                            </button>
-                        </div>
-                        <div className="flex items-center mt-2 rounded-lg px-1 py-1 cursor-pointer">
-                            <div className="relative flex flex-shrink-0 items-end">
-                                <Image className="h-16 w-16 rounded-full" src="https://picsum.photos/id/1027/150/150" width={50} height={50}/>
-                                <span className="absolute h-4 w-4 bg-green-400 rounded-full bottom-0 right-0 border-2 border-white"></span>
+                        <div className="bg-white  px-5 py-3.5 rounded-lg shadow hover:shadow-xl max-w-sm mx-auto mb-3">
+                            <div className="w-full flex items-center justify-between">
+                                <span className="font-medium text-sm text-slate-400">New Notification</span>
+                                <button className="-mr-1 bg-slate-100 hover:bg-slate-200 text-slate-400 hover:text-slate-600 h-5 w-5 rounded-full flex justify-center items-center">
+                                    <svg className="h-2 w-2 fill-current items-center" viewBox="0 0 20 20"><path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/></svg>
+                                </button>
                             </div>
-                            <div className="ml-3">
-                                <span className="font-semibold tracking-tight text-xs">Jane Doe</span>
-                                <span className="text-xs leading-none opacity-50">reacted to your comment:</span>
-                                <p className="text-xs leading-4 pt-2 italic opacity-70">"This is the comment..."</p>
-                                <span className="text-[10px] text-blue-500 font-medium leading-4 opacity-75">a few seconds ago</span>
+                            <div className="flex items-center mt-2 rounded-lg px-1 py-1 cursor-pointer">
+                                <div className="relative flex flex-shrink-0 items-end">
+                                    <Image className="h-16 w-16 rounded-full" src="https://picsum.photos/id/1027/150/150" width={50} height={50}/>
+                                    <span className="absolute h-4 w-4 bg-green-400 rounded-full bottom-0 right-0 border-2 border-white"></span>
+                                </div>
+                                <div className="ml-3">
+                                    <span className="font-semibold tracking-tight text-xs">Jane Doe</span>
+                                    <span className="text-xs leading-none opacity-50">reacted to your comment:</span>
+                                    <p className="text-xs leading-4 pt-2 italic opacity-70">"This is the comment..."</p>
+                                    <span className="text-[10px] text-blue-500 font-medium leading-4 opacity-75">a few seconds ago</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="bg-white  px-5 py-3.5 rounded-lg shadow hover:shadow-xl max-w-sm mx-auto mb-3">
+                            <div className="w-full flex items-center justify-between">
+                                <span className="font-medium text-sm text-slate-400">New Notification</span>
+                                <button className="-mr-1 bg-slate-100 hover:bg-slate-200 text-slate-400 hover:text-slate-600 h-5 w-5 rounded-full flex justify-center items-center">
+                                    <svg className="h-2 w-2 fill-current items-center" viewBox="0 0 20 20"><path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/></svg>
+                                </button>
+                            </div>
+                            <div className="flex items-center mt-2 rounded-lg px-1 py-1 cursor-pointer">
+                                <div className="relative flex flex-shrink-0 items-end">
+                                    <Image className="h-16 w-16 rounded-full" src="https://picsum.photos/id/1027/150/150" width={50} height={50}/>
+                                    <span className="absolute h-4 w-4 bg-green-400 rounded-full bottom-0 right-0 border-2 border-white"></span>
+                                </div>
+                                <div className="ml-3">
+                                    <span className="font-semibold tracking-tight text-xs">Jane Doe</span>
+                                    <span className="text-xs leading-none opacity-50">reacted to your comment:</span>
+                                    <p className="text-xs leading-4 pt-2 italic opacity-70">"This is the comment..."</p>
+                                    <span className="text-[10px] text-blue-500 font-medium leading-4 opacity-75">a few seconds ago</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="bg-white  px-5 py-3.5 rounded-lg shadow hover:shadow-xl max-w-sm mx-auto mb-3">
+                            <div className="w-full flex items-center justify-between">
+                                <span className="font-medium text-sm text-slate-400">New Notification</span>
+                                <button className="-mr-1 bg-slate-100 hover:bg-slate-200 text-slate-400 hover:text-slate-600 h-5 w-5 rounded-full flex justify-center items-center">
+                                    <svg className="h-2 w-2 fill-current items-center" viewBox="0 0 20 20"><path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/></svg>
+                                </button>
+                            </div>
+                            <div className="flex items-center mt-2 rounded-lg px-1 py-1 cursor-pointer">
+                                <div className="relative flex flex-shrink-0 items-end">
+                                    <Image className="h-16 w-16 rounded-full" src="https://picsum.photos/id/1027/150/150" width={50} height={50}/>
+                                    <span className="absolute h-4 w-4 bg-green-400 rounded-full bottom-0 right-0 border-2 border-white"></span>
+                                </div>
+                                <div className="ml-3">
+                                    <span className="font-semibold tracking-tight text-xs">Jane Doe</span>
+                                    <span className="text-xs leading-none opacity-50">reacted to your comment:</span>
+                                    <p className="text-xs leading-4 pt-2 italic opacity-70">"This is the comment..."</p>
+                                    <span className="text-[10px] text-blue-500 font-medium leading-4 opacity-75">a few seconds ago</span>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
-           {/* /!*end: Social Network notification:light*!/*/}
-
 
                 <footer className="sticky bottom-0 left-0 z-20 p-4 w-full bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
                     <div className='flex mb-.5'>
